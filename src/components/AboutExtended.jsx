d3c5e8d35a1786e65f509b1135a9005046403583
XX <fallwall19@gmail.com>
2020-05-27 14:21:14 -0700

    main bio css adjust

--- src/components/AboutExtended.jsx
+++ src/components/AboutExtended.jsx
@@ -6,6 +6,14 @@ const AboutExtended = () => {
       <p>Curious and having a wide range of interests, Tibby pursues creative work that is insight-driven, strongly articulated, and intimately considered.</p>
       <p>With a focus on educational, cultural, and socially responsible projects, Tibby is eager to work with people and on projects that create value in exceptional ways.</p>
       <p>Tibby is currently a freelance developer and designer, living her bicoastal dream between New York City and Vancouver.</p>
+      <br />
+      <br />
+      <a href="mailto:sherunsfreely@gmail.com">Email</a>
+      <a href="https://github.com/fallwall">GitHub</a>
+      <a href="https://www.linkedin.com/in/tibbyxu/">LinkedIn</a>
+      <a href="https://www.instagram.com/fallwall19/">Instagram</a>
+      <a href="https://www.sherunsfreely.com">SheRunsFreely</a>
+  
     </div>
   )
 }
--- src/components/main/Bio.jsx
+++ src/components/main/Bio.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Bio=()=> {
   return (
     <div className="bio">
-      <p>Tibby Xu is a web developer and designer based in New York City and Vancouver.</p>
+      <p>Tibby Xu is a web developer and designer<br />currently based in New York City and Vancouver.</p>
     </div>
   )
 }
