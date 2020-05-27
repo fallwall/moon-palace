528e5f1d0706fc0c6572fc656a6bf6b09bd660cc
XX <fallwall19@gmail.com>
2020-05-27 15:34:33 -0700

    header coordinates change on header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const coordinates = [
+  { "city": "nyc", "coord": "40°39′N 73°56′W" },
+  { "city": "yvr", "coord": "49°15′N 123°6′W" },
+  { "city": "hgh", "coord": "30°16′N 120°9′E" }
+]
 
 const Header = () => {
+  const [location, setLocation] = useState("40°39′N 73°56′W");
+  setTimeout(() => {
+    setLocation(coordinates[Math.floor(Math.random() * 3)].coord)
+  }, 3000);
   return (
     <div className="header">
       <div className="header-left">
         Tibby Xu
       </div>
       <div className="header-right">
-        - 40°39′N 73°56′W
+        - {location}
       </div>
     </div>
   )
