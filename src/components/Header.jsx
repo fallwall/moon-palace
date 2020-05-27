5c5012658eca80e60729a012429e1fb9fa356310
XX <fallwall19@gmail.com>
2020-05-27 11:18:17 -0700

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="header">
-
+      <div className="header-left">
+        Tibby Xu
+      </div>
+      <div className="header-right">
+        - 40°39′N 73°56′W
+      </div>
     </div>
   )
 }
