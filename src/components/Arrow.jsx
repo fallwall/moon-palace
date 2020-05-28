ca8ff98e0617f4f6e505174c313e6ecb72de147b
XX <fallwall19@gmail.com>
2020-05-28 14:28:50 -0700

    svg arrow filled in

--- src/components/Arrow.jsx
+++ src/components/Arrow.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Arrow = () => {
   return (
     <div className="arrow-container">
-      <i class="arrow up"></i>
+      <svg class="arrow" x="0px" y="0px" width="106px" height="40px" viewBox="0 0 53 20" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M11 2.206l-6.235 7.528-.765-.645 7.521-9 7.479 9-.764.646-6.236-7.53v21.884h-1v-21.883z"/></svg>
     </div>
   )
 }
