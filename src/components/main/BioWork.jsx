108d7439937785348f327ad4773d2795866a1012
XX <fallwall19@gmail.com>
2020-05-29 13:33:42 -0700

    worklist main page hover logic and display

--- src/components/main/BioWork.jsx
+++ src/components/main/BioWork.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import data from '../port/portfolio.json';
 
 const BioWork = () => {
+  const [hover, setHover] = useState(null);
   return (
     <div className="bio-work">
-      <div className="bio-work-each">
-        zeug
-      </div>
-      <div className="bio-work-each">
-        zeug
-      </div>
-      <div className="bio-work-each">
-        zeug
-      </div>
-      <div className="bio-work-each">
-        zeug
-      </div>
+      {data.data.map(e =>
+        <div
+          key={e.id}
+          onMouseOver={()=>setHover(e.id)}
+          className="bio-work-each">
+          <img src={e.img} className={e.id === hover ? "hidden":""}/>
+          <div className={e.id === hover ? "centered":"hidden"}>
+            {e.title}
+          </div>
+        </div>)}
     </div>
   )
 }
---
+++ src/components/work/WorkList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import data from '../port/portfolio.json';
+
+const WorkList = () => {
+  return (
+    <div className="work-list">
+     
+    </div>
+  )
+}
+
+export default WorkList;
