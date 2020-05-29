fefcf34ad528eec62058fa13232ead7687536804
XX <fallwall19@gmail.com>
2020-05-29 15:18:58 -0700

    workpage imgs insert

--- src/components/work/WorkPage.jsx
+++ src/components/work/WorkPage.jsx
@@ -9,9 +9,28 @@ const testData = data.data[0];
 
 const WorkPage = () => {
   return (
-    <div className="work-page">
-      <div className="work-page-cover">
+    <div className="workpage">
+      <div className="workpage-cover">
         <img src={testData.img} alt="current project cover" />
+      </div>
+      <div className="workpage-title">
+        <h1>{testData.title}</h1>
+      </div>
+      {/* !!!!!!!below is project summary!!!!!!! */}
+      <div className="workpage-summary">
+        <div className="workpage-summary-tools">
+          <div><ul>{testData.tools.map(t => <li>{t}</li>)}</ul></div>
+          <div><p>{testData.cat}</p></div>
+        </div>
+        <div className="workpage-summary-desc">
+          <div>
+            {testData.desc}
+          </div>
+        </div>
+      </div>
+      {/* !!!!!!!above is project summary!!!!!!! */}
+      <div className="workpage-imgs">
+        {testData.imgs.map(i => <img src={i} alt="more about current project" />)}
       </div>
       <PrevNext />
     </div>
