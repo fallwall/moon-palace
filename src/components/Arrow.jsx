5304b0d4c3d8c3e7f3e9da91667556ba95ced5f0
XX <fallwall19@gmail.com>
2020-05-29 12:33:48 -0700

    arrow to top

--- src/components/Arrow.jsx
+++ src/components/Arrow.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+const toTop = () => {
+  window.scrollTo(0, 0);
+}
 
 const Arrow = () => {
   return (
-    <div className="arrow-container">
-      <svg class="arrow" x="0px" y="0px" width="106px" height="40px" viewBox="0 0 53 20" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M11 2.206l-6.235 7.528-.765-.645 7.521-9 7.479 9-.764.646-6.236-7.53v21.884h-1v-21.883z"/></svg>
+    <div className="arrow-container" onClick={toTop}>
+      <svg class="arrow" x="0px" y="0px" width="106px" height="40px" viewBox="0 0 53 20" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M11 2.206l-6.235 7.528-.765-.645 7.521-9 7.479 9-.764.646-6.236-7.53v21.884h-1v-21.883z" /></svg>
     </div>
   )
 }
