ed798edef0e2b82be68502e421b9be2c0f084c28
XX <fallwall19@gmail.com>
2020-05-29 15:27:23 -0700

    slides and desc in workpage

--- src/components/work/WorkPage.jsx
+++ src/components/work/WorkPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState} from 'react';
 import PrevNext from './PrevNext';
 
 import data from '../port/portfolio.json';
@@ -6,8 +6,14 @@ import data from '../port/portfolio.json';
 import './WorkPage.css';
 
 const testData = data.data[0];
+const testSlides = testData.slides;
 
 const WorkPage = () => {
+  const [slide, setSlide] = useState(0);
+  setTimeout(() => {
+    slide < testSlides.length-1 ? setSlide(slide + 1) : setSlide(0);
+  }, 10000);
+
   return (
     <div className="workpage">
       <div className="workpage-cover">
@@ -31,6 +37,12 @@ const WorkPage = () => {
       {/* !!!!!!!above is project summary!!!!!!! */}
       <div className="workpage-imgs">
         {testData.imgs.map(i => <img src={i} alt="more about current project" />)}
+      </div>
+      <div className="workpage-content">
+        {testData.content}
+      </div>
+      <div className="workpage-slides">
+        <img src={testSlides[slide]} alt="on ratotation display" />
       </div>
       <PrevNext />
     </div>
