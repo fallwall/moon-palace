cc244ce09ea0a1a78a151379f3860d1176c96747
XX <fallwall19@gmail.com>
2020-06-11 18:47:14 -0700

    LENGTH in workpage and prevnext

--- src/components/work/PrevNext.jsx
+++ src/components/work/PrevNext.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
+
 const PrevNext = (props) => {
+  const LENGTH = props.length;
   return (
     <div className="workpage-prevnext">
-      <div className="prev" onClick={() => props.workItem < 2 ? props.changeWorkItem(10): props.changeWorkItem(props.workItem - 1)}>
+      <div className="prev" onClick={() => props.workItem < 2 ? props.changeWorkItem(LENGTH): props.changeWorkItem(props.workItem - 1)}>
         <div><p>Previous</p></div>
         <div><h1>{props.prevName}</h1></div>
       </div>
-      <div className="next" onClick={() => props.workItem > 9 ? props.changeWorkItem(1): props.changeWorkItem(props.workItem + 1)}>
+      <div className="next" onClick={() => props.workItem > (LENGTH-1) ? props.changeWorkItem(1): props.changeWorkItem(props.workItem + 1)}>
         <div><h1>{props.nextName}</h1></div>
         <div><p>Next</p></div>
       </div>
--- src/components/work/WorkPage.jsx
+++ src/components/work/WorkPage.jsx
@@ -10,6 +10,8 @@ const WorkPage = (props) => {
   const [slide, setSlide] = useState(0);
   const testData = data.data[props.workItem - 1];
   const testSlides = testData.slides;
+  const LENGTH = data.data.length;
+  // 10
   
   setTimeout(() => {
     slide < testSlides.length - 1 ? setSlide(slide + 1) : setSlide(0);
@@ -26,7 +28,7 @@ const WorkPage = (props) => {
       {/* !!!!!!!below is project summary!!!!!!! */}
       <div className="workpage-summary">
         <div className="workpage-summary-tools">
-          <div><ul>{testData.tools.map(t => <li>{t}</li>)}</ul></div>
+          <div><ul>{testData.tools.map(t => <li key={t}>{t}</li>)}</ul></div>
           <div><p>{testData.cat}</p></div>
         </div>
         <div className="workpage-summary-desc">
@@ -37,7 +39,7 @@ const WorkPage = (props) => {
       </div>
       {/* !!!!!!!above is project summary!!!!!!! */}
       <div className="workpage-imgs">
-        {testData.imgs && testData.imgs.map(i => <img src={i} alt="more about current project" />)}
+        {testData.imgs && testData.imgs.map((i,a) => <img key={a} src={i} alt="more about current project" />)}
       </div>
       <div className="workpage-content">
         {testData.content && testData.content}
@@ -47,9 +49,11 @@ const WorkPage = (props) => {
       </div>
       <PrevNext
         workItem={props.workItem}
-        prevName={props.workItem > 3 ? data.data[props.workItem-2].title: data.data[9].title}
-        nextName={props.workItem < 10 ? data.data[props.workItem].title : data.data[0].title}
-        changeWorkItem={props.changeWorkItem} />
+        prevName={props.workItem > 3 ? data.data[props.workItem - 2].title : data.data[LENGTH - 1].title}
+        nextName={props.workItem < LENGTH ? data.data[props.workItem].title : data.data[0].title}
+        changeWorkItem={props.changeWorkItem}
+        length={LENGTH}
+      />
     </div>
   )
 }
